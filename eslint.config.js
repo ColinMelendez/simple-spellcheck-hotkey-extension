526eba3462fe6b98c4088c01af4bdb3e3445341a
Colin <cwrmcolin@gmail.com>
2025-06-17 17:50:31 -0700

    feat(project): enhanced linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,67 @@
 import antfu from '@antfu/eslint-config';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
+import oxlint from 'eslint-plugin-oxlint';
+
+// TIP: to view which rules are enabled, run `pnpx @eslint/config-inspector`
 
 export default antfu({
-  // formatters: true,
+  // additional plugins
+  plugins: {
+    'better-tailwindcss': eslintPluginBetterTailwindcss,
+  },
+  settings: {
+    'better-tailwindcss': {
+      entryPoint: 'assets/tailwind.css',
+    },
+  },
+  // Use prettier to format files that eslint can not handle (html, css, markdown)
+  formatters: {
+    html: 'prettier',
+    css: 'prettier',
+    markdown: 'prettier',
+  },
+  // Define path to tsconfig to enable type-aware rules
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
+  // Enable react rules
   react: true,
+  // Modify or add specific rule settings for eslint rules & packages covered by antfu-eslint
   rules: {
     // allow console.logs
     'no-console': 'off',
+
     // enforce semicolons at the end of statements
     'style/semi': ['off', 'always'],
+
+    // lambda functions should always use argument parenthesis (two overlapping rules)
+    'style/arrow-parens': ['error', 'always'],
+
+    // I prefer to use arrow-functions at all times
+    'antfu/top-level-function': 'off',
+
+    // I prefer to control when I omit "new" when throwing errors
+    'unicorn/throw-new-error': 'off',
+
+    // make unused values warnings instead of errors for less visual noise while coding
+    'unused-imports/no-unused-vars': 'warn',
+
+    // use the recommended rules from the better-tailwindcss plugin
+    ...eslintPluginBetterTailwindcss.configs.recommended.rules,
+
+    // break tailwindcss classnames into multiple lines when they have different group prefixes
+    'better-tailwindcss/multiline': ['warn', {
+      printWidth: 200,
+      classesPerLine: 0,
+      group: 'newLine',
+    }],
+
+    // force all external variables using in tailwindcss classnames to use the same syntax
+    'better-tailwindcss/enforce-consistent-variable-syntax': ['warn', {
+      syntax: 'arbitrary',
+    }],
   },
-});
+}).append(
+  // oxlint compat plugin needs to come after everything else so that it can properly override rules
+  oxlint.buildFromOxlintConfigFile('./.oxlintrc.json'),
+);
