f86f931ef49b9bef8ec52bc49d587b6c763a73e0
Colin <cwrmcolin@gmail.com>
2025-06-30 23:03:44 -0700

    dev(domain): fleshed out global defaults

--- lib/Domain/global-defaults.ts
+++ lib/Domain/global-defaults.ts
@@ -2,9 +2,25 @@
  * @file this file contains statically-defined default values and constants that are globally relevant
  */
 
-import { ScrambleDensity } from './settings-schema';
+import * as Schema from 'effect/Schema';
+import {
+  ScrambleDensity,
+  Settings,
+} from './settings-schema';
 
 /**
  * Default value for the density of the text scrambling.
  */
 export const DEFAULT_SCRAMBLE_DENSITY = ScrambleDensity.make(0.7);
+
+/**
+ * The key used to store the settings in all storage areas.
+ */
+export const SETTINGS_STORAGE_KEY = 'settings';
+
+/**
+ * The default settings object.
+ */
+export const DEFAULT_SETTINGS = Schema.decodeSync(Settings)({
+  scrambleDensity: DEFAULT_SCRAMBLE_DENSITY,
+});
