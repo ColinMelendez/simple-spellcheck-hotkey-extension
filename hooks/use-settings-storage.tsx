587ff8ae16f847cea0538bd0ba283e09461154ac
Colin <cwrmcolin@gmail.com>
2025-06-30 23:27:14 -0700

    refactor(popup): use settings storage hook using effect
    Obviously this is somewhat forced as a matter of interest rather than technical merit, and at the moment, any errors are just being logged and not handled, but I must admit that the result is easier to reason about.

--- hooks/use-settings-storage.tsx
+++ hooks/use-settings-storage.tsx
@@ -1,43 +1,70 @@
 import type { Browser } from '#imports';
 import { browser } from '#imports';
+import * as Effect from 'effect/Effect';
 import * as Schema from 'effect/Schema';
 import { useCallback, useEffect, useState } from 'react';
 import { DEFAULT_SETTINGS, SETTINGS_STORAGE_KEY } from '@/lib/domain/global-defaults';
 import { Settings } from '@/lib/domain/settings-schema';
+import { popupRuntime } from '@/lib/runtimes/react-runtimes';
+import { BrowserLocalStorage } from '@/lib/services/browser-local-storage';
 
 /**
- * A hook to subscribe-to and update the state of the extension's stored settings
+ * Update the settings state in storage with a new value
+ * @param updatedSettings - The new settings value to store
+ */
+const updateSettingsStorage = (updatedSettings: Settings) => {
+  void Effect.gen(function* () {
+    const localStorage = yield* BrowserLocalStorage;
+    const value = yield* Schema.decodeUnknown(Settings)(updatedSettings)
+    yield* localStorage.use(async (storage) => {
+      void storage.set({
+        [SETTINGS_STORAGE_KEY]: value,
+      });
+    });
+  }).pipe(
+    Effect.catchTags({
+      BrowserLocalStorageError: (error) => Effect.logError(error),
+      ParseError: (error) => Effect.logError(error),
+    }),
+    popupRuntime.runPromise,
+  );
+}
+
+/**
+ * A hook to subscribe to and update the state of the extension's stored settings
  * @returns A tuple containing the current settings state and a function to update the settings state in storage
  */
 export const useSettingsStorage = () => {
   const [settings, setSettings] = useState(DEFAULT_SETTINGS);
 
   const onLocalStorageSettingsChange = useCallback((changes: Record<string, Browser.storage.StorageChange>) => {
-    if (changes[SETTINGS_STORAGE_KEY]) {
-      try {
-        setSettings(Schema.decodeUnknownSync(Settings)(changes[SETTINGS_STORAGE_KEY].newValue));
+    void Effect.gen(function* () {
+      if (changes[SETTINGS_STORAGE_KEY]) {
+        const value = yield* Schema.decodeUnknown(Settings)(changes[SETTINGS_STORAGE_KEY].newValue)
+        setSettings(value);
       }
-      catch (error) {
-        console.error('Error decoding settings from storage', error);
-      }
-    }
+    }).pipe(
+      Effect.catchTags({
+        ParseError: (error) => Effect.logError(error),
+      }),
+      popupRuntime.runPromise,
+    );
   }, []);
 
   // On initial load, get the latest settings from storage and listen for changes
   useEffect(() => {
-    browser.storage.local.get(SETTINGS_STORAGE_KEY)
-      .then((data: Record<string, unknown>) => {
-        console.log('data', data);
-        try {
-          setSettings(Schema.decodeUnknownSync(Settings)(data[SETTINGS_STORAGE_KEY]));
-        }
-        catch (error) {
-          console.error('Error decoding settings from storage', error);
-        }
-      })
-      .catch((err) => {
-        console.error('Could not update settings from storage, using default values', err);
-      });
+    void Effect.gen(function* () {
+      const localStorage = yield* BrowserLocalStorage;
+      const value = yield* localStorage.use(async (storage) => storage.get(SETTINGS_STORAGE_KEY))
+      const decoded = yield* Schema.decodeUnknown(Settings)(value[SETTINGS_STORAGE_KEY])
+      setSettings(decoded)
+    }).pipe(
+      Effect.catchTags({
+        ParseError: (error) => Effect.logError(error),
+        BrowserLocalStorageError: (error) => Effect.logError(error),
+      }),
+      popupRuntime.runPromise,
+    );
 
     browser.storage.local.onChanged.addListener(onLocalStorageSettingsChange);
 
@@ -45,18 +72,6 @@ export const useSettingsStorage = () => {
       browser.storage.local.onChanged.removeListener(onLocalStorageSettingsChange);
     };
   }, [onLocalStorageSettingsChange]);
-
-  /**
-   * Update the settings state in storage with a new value
-   * @param updatedSettings - The new settings value to store
-   */
-  const updateSettingsStorage = useCallback((updatedSettings: Settings) => {
-    browser.storage.local.set({
-      [SETTINGS_STORAGE_KEY]: Schema.decodeSync(Settings)(updatedSettings),
-    }).catch((err) => {
-      console.error('Error updating settings', err);
-    });
-  }, []);
 
   return [
     settings,
