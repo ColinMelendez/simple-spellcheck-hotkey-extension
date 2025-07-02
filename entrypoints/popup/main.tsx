992cf3dda1bfb32648387fdfe3fbef57585aae4a
Colin <cwrmcolin@gmail.com>
2025-07-01 20:50:19 -0700

    feat(popup): system-based dark-light modes

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@/components/theme-provider.tsx';
 import App from './app.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="system">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
